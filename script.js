d1359bbe0623c44e2648bae9ea3a09aa37c36600
Emily Aguirre <emgal.aguirre@gmail.com>
2020-08-11 22:17:37 -0500

    created foundations of html, css and outlined questions

---
+++ script.js
@@ -0,0 +1,29 @@
+// coding questions
+// Insied which HTML element do we put the Javascript?
+// <javascript>
+// <scripting>
+// <scrip>
+// <js>
+
+// How do you write "Hello World" in an alert box?
+// msg("Hello World")
+// alert("Hello World")
+// alertBox("Hello World")
+// msgBox("Hello World")
+
+// How do you create a function in Javascript 
+// function myFunction ()
+// function = myFunction ()
+// function: myFunction ()
+
+// How do you write an if statement in JavaScript 
+// if i = 0 
+// if i = 0 then 
+// if (i = 0)
+// if i === 0 then
+
+// How does a for loop start?
+// for (i=0; i <=5)
+// for (i = 0; i <= 5; i++)
+// for (i<5;i++)
+// for i = 1 to 5
